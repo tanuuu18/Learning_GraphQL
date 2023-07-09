28d0e12d658e7e753931326e2aacd90aa68767b8
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-07-09 07:44:00 +0000

    3. Create Resolvers anf functions

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+//db
+import db from "./_db.js";
+
 //types
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+      games() {
+        return db.games;
+      },
+      reviews() {
+        return db.reviews;
+      },
+      authors() {
+        return db.authors;
+      },
+    },
+  };
+
+// games{
+//     title
+// }
 
 //server setup
 const server = new ApolloServer({
   //typeDefs --> definitions of types of data
   typeDefs,
   //resolvers
+  resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
